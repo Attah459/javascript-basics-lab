ba26a0f5150d4e284e81ed9b945f7309f597d90f
Attah459 <n01766643@humber.ca>
2025-05-19 19:59:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let studentName  = 'Attah Attah';
+let studentAge = 25;
+let isEnrolled = true;
+console.log (studentName);
+console.log (studentAge);
+console.log (isEnrolled);
+console.log ("");
+console.log ("");
+let Name  = 'John Dogma';
+let age = 104;
+let isHappy = false;
+let fave_food = [];
+fave_food.push("Beans","rice","chicken");
+
+const person={
+ Name:'Bob Dogma',
+ age : 78,
+ isHappy : true,
+
+}
+
+
+console.log (Name);
+console.log (age);
+console.log (isHappy);
+console.log (fave_food);
+console.log (person)
+console.log ("");
+console.log ("");
+console.log (age +2);
+console.log (age < 3);
+console.log (age < 3 || age == 3 );
